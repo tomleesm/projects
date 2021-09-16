645e75383d12a4accacc033f7350666d13e49d75
Tom <tomleesm@gmail.com>
2021-09-16 21:26:29 +0800

    new project deployment

---
+++ todo-list/precache-manifest.7884c4ed774403e424d4018acc49bc87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab24016525b07c84b02b62434249588",
+    "url": "index.html"
+  },
+  {
+    "revision": "482aba3341882daf6b7f",
+    "url": "js/app.5a4c5e1d.js"
+  },
+  {
+    "revision": "b561c2809c4a53055b61",
+    "url": "js/chunk-vendors.39ac3317.js"
+  },
+  {
+    "revision": "0e14d6be4bd825c00bd02ef3c8d789b7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
