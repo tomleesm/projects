c81bf23843e031a4823942367f66307c957fd3b6
Tom <tomleesm@gmail.com>
2021-09-18 01:10:23 +0800

    new project deployment

---
+++ home/precache-manifest.504a9fd552bda6285ccc2086cc9a6935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3296324d2fcd071c9e95",
+    "url": "css/chunk-vendors.63c5e34a.css"
+  },
+  {
+    "revision": "9865b00c5fced2db016902b0039195fc",
+    "url": "index.html"
+  },
+  {
+    "revision": "30c88183a45a5b5a692d",
+    "url": "js/app.78303c38.js"
+  },
+  {
+    "revision": "3296324d2fcd071c9e95",
+    "url": "js/chunk-vendors.d2478309.js"
+  },
+  {
+    "revision": "0e14d6be4bd825c00bd02ef3c8d789b7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
