5fe0199d0ecb81dfbe1fcbaa67e27e85931901d5
Tom <tomleesm@gmail.com>
2021-09-17 21:56:41 +0800

    基本的新增、修改、顯示 todo list

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,3 +1,62 @@
 <template>
   <h1>Todo List</h1>
+  <div>
+    <p><button type="button" @click="addTask">add task</button></p>
+
+    <h2>未完成</h2>
+    <ul>
+      <li v-for="task in notDoneTaskList" :key="task.id">
+        <input type="checkbox" v-model="task.done">
+        <input type="text" v-model="task.content">
+      </li>
+    </ul>
+
+    <h2>完成</h2>
+    <ul>
+      <li v-for="task in doneTaskList" :key="task.id">
+        <input type="checkbox" v-model="task.done">
+        <label><del>{{ task.content }}</del></label>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { v1 as uuidv1 } from 'uuid'
+
+export default {
+  data() {
+    return {
+      taskList: []
+    }
+  },
+  computed: {
+    notDoneTaskList() {
+      return this.taskList.filter((task, index) => {
+        return !task.done
+      })
+    },
+    doneTaskList() {
+      return this.taskList.filter((task, index) => {
+        return task.done
+      })
+    }
+  },
+  methods: {
+    addTask() {
+      let newTask = {
+        id: uuidv1(),
+        content: '',
+        done: false
+      }
+
+      this.taskList.unshift(newTask)
+
+      // render 後，滑鼠游標在 taskList 的第一個
+      this.$nextTick(() => {
+        // 選取 taskList 的第一個元素，並聚焦在其 content 欄位
+      })
+    }
+  }
+}
+</script>
