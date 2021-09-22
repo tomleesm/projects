b2946cd3d0aef608c0e07ebfa74c9051e9dca00d
Tom <tomleesm@gmail.com>
2021-09-22 17:23:43 +0800

    路由設定完成

--- wiki/src/App.vue
+++ wiki/src/App.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Wiki</h1>
+  <router-view></router-view>
 </template>
---
+++ wiki/src/components/Articles.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Articles</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
