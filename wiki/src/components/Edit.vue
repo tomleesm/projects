93660ad20122f88c0d75fcded7f2802c51c2d80e
Tom <tomleesm@gmail.com>
2021-09-23 18:07:06 +0800

    編輯區 Edit/Preview 導覽列，和預覽功能完成

--- wiki/src/components/Edit.vue
+++ wiki/src/components/Edit.vue
@@ -1,10 +1,32 @@
 <template>
   <h1>{{ article.title }}</h1>
+
+  <a href="#edit" @click.prevent="preview = false">Edit</a> | <a href="#preview" @click.prevent="preview = true">Preview</a>
+
+  <div class="preview" v-if="preview" v-html="html">
+  </div>
+  <div v-else>
+    <textarea v-model="content"></textarea>
+  </div>
 </template>
 
 <script>
+import MarkdownIt from 'markdown-it'
+
 export default {
-  props: ['article']
+  props: ['article'],
+  computed: {
+    html () {
+      const md = new MarkdownIt()
+      return md.render(this.content)
+    }
+  },
+  data () {
+    return {
+      content: this.article.content,
+      preview: false
+    }
+  }
 }
 </script>
 
