e239365aa4491e0576bd716627a7d75847805557
Tom <tomleesm@gmail.com>
2021-09-27 21:03:35 +0800

    setup Plyr

--- video-player/src/App.vue
+++ video-player/src/App.vue
@@ -1,4 +1,22 @@
 <template>
   <h1>Video Player</h1>
-  <button type="button" class="btn btn-primary">Button</button>
+  <!-- if video.mp4 put in src/, <video src="./video.mp4"> -->
+  <video id="player" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" controls>
+  </video>
 </template>
+
+<script>
+import 'plyr/dist/plyr.css'
+import Plyr from 'plyr'
+
+export default {
+  data () {
+    return {
+      player: {}
+    }
+  },
+  mounted () {
+    this.player = new Plyr(document.getElementById('player'))
+  }
+}
+</script>
