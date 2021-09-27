2748b329529329aab8c0c087a23aa4a21bec3527
Tom <tomleesm@gmail.com>
2021-09-27 23:37:44 +0800

    new project deployment

---
+++ video-player/precache-manifest.1bb10abbc5d18d386e2f047584bcc6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff69ad79461f010e51b5",
+    "url": "css/chunk-vendors.c305d2a6.css"
+  },
+  {
+    "revision": "284e46506382b3dd43f8f257597c5c9e",
+    "url": "index.html"
+  },
+  {
+    "revision": "1936932073a09fee3073",
+    "url": "js/app.168a1c0a.js"
+  },
+  {
+    "revision": "ff69ad79461f010e51b5",
+    "url": "js/chunk-vendors.52fb99dd.js"
+  },
+  {
+    "revision": "0e14d6be4bd825c00bd02ef3c8d789b7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
