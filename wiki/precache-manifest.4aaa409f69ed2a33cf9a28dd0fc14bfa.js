f32ee24ccc771456ad5c89d3585f4a51ffbfee6e
Tom <tomleesm@gmail.com>
2021-09-27 14:50:08 +0800

    new project deployment

---
+++ wiki/precache-manifest.4aaa409f69ed2a33cf9a28dd0fc14bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e9512285ad57e22710",
+    "url": "css/chunk-vendors.63c5e34a.css"
+  },
+  {
+    "revision": "342acdbef5a7ac14776a3e3417254324",
+    "url": "index.html"
+  },
+  {
+    "revision": "8607aee9d0e73a2717e0",
+    "url": "js/app.27957871.js"
+  },
+  {
+    "revision": "16e9512285ad57e22710",
+    "url": "js/chunk-vendors.85eba8f4.js"
+  },
+  {
+    "revision": "0e14d6be4bd825c00bd02ef3c8d789b7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
