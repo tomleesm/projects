9ecdacffff79b83c288a382ce4ad18c7ccea6669
Tom <tomleesm@gmail.com>
2021-10-26 15:46:11 +0800

    set baseUrl in cypress.json

---
+++ bug-report/cypress/integration/issue.js
@@ -0,0 +1,14 @@
+describe('單一 issue 相關的測試', () => {
+  it('新增一個 issue', () => {
+    cy.visit('/')
+    // click button "New issue"
+    // show issue create page
+    //   必須輸入標題
+    //   Edit 分頁輸入 markdown 語法
+    //   點選 Preview 分頁，會自動 render 成 HTML
+    //   右側指定 issue 給誰
+    //   新增 Label
+    //   加上 Label
+    //   點選按鈕 Commit 後，顯示 issue
+  })
+})
